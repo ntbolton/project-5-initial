cd130c8d4da1d5912e5574b3da3aefad6f4b6971
ntbolton <ntbolton03@gmail.com>
2023-12-07 10:45:35 -0700

    implemented cocktail shaker sort and gnome sort

--- src/main/java/com/example/sorting/SortingUtility.java
+++ src/main/java/com/example/sorting/SortingUtility.java
@@ -6,12 +6,45 @@ public class SortingUtility {
     public static <T extends Comparable<T>> void gnomeSort(T[] data) {
 
         // TODO implement Gnome Sort here
+        int pos = 0;
+
+        while (pos < data.length) {
+            if ((pos == 0) || (data[pos].compareTo(data[pos-1]) >= 0) ) {
+                pos++;
+            } else {
+                swap(data, pos, pos - 1);
+                pos--;
+            }
+        }
+
     }
 
 
     public static <T extends Comparable<T>> void cocktailShakerSort(T[] data) {
 
         // TODO implement Cocktail Shaker Sort here
+        boolean swapped = false;
+        do {
+            for (int i = 0; i < data.length - 1; i++) {
+                if (data[i].compareTo(data[i + 1]) > 0) {
+                    swap(data, i, i + 1);
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+            swapped = false;
+            for (int i = data.length - 2; i >= 0; i--) {
+                if (data[i].compareTo(data[i + 1]) > 0) {
+                    swap(data, i, i + 1);
+                    swapped = true;
+                }
+            }
+
+        } while (swapped);
+
+
     }
 
 
